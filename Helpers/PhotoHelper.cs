f5ce15fb902ac9c88f1dfd02ada0cf3e075e4ffe
alex.zh <alex.zh@genie-solution.com>
2019-02-28 21:41:36 +0200

    .

---
+++ Helpers/PhotoHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HashCode2019.Model;
+
+namespace HashCode2019.Helpers
+{
+    public static class PhotoHelper
+    {
+        public static PhotoParameters ToHorizontalPhoto(PhotoParameters p1, PhotoParameters p2)
+        {
+            var tags = p1.Tags.Union(p2.Tags).ToList();
+            return new PhotoParameters()
+            {
+                Id = String.Join(' ', p1.Id, p2.Id),
+                Orientation = Orientation.Horizontal,
+                Tags = tags,
+                TagCount = tags.Count
+            };
+        }
+    }
+}
